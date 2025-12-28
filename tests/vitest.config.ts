753e4c12882506eeb40554bea6b508f29bb507ed
Ravi Shankar Saini <sainiravi015@gmail.com>
2025-12-28 20:25:25 +0530

    added codecov bundles

--- tests/vitest.config.ts
+++ tests/vitest.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vitest/config';
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 export default defineConfig({
-    test: {
-        environment: 'jsdom',
-        globals: true,
-        setupFiles: ['./setup.ts'],
-        coverage: {
-            reporter: ['text', 'json-summary', 'json'],
-        },
-        reporters: ['default', 'junit'],
-        outputFile: '../test-results.xml',
-    },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./setup.ts'],
+    reporters: ['junit'],
+    outputFile: '../test-results.xml',
+  },
+  plugins: [
+    // Put the Codecov vite plugin after all other plugins
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "openseadragon-capture",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ]
 });
